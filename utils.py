a819d8d61ca6f1c0d831d4bc0af9e040212b08b6
Krisanthi Segar <krisanthi.20232384@iit.ac.lk>
2026-04-05 11:00:19 +0530

    refactor: update haversine return statement with new radius constant

--- utils.py
+++ utils.py
@@ -25,4 +25,4 @@ def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> fl
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    return R * c
+    return EARTH_RADIUS_KM * c
